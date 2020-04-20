01411cf47da66921978b6d5212dc4723d772e0e7
Surajit Paul <surajit.paul@ericsson.com>
2020-04-20 20:04:43 +0530

    adding initial 3 files

---
+++ index.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+# https://www.wintellect.com/containerize-python-app-5-minutes/
+
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello World!"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
